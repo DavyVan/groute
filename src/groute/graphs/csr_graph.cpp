a8db45f08653f5ab34d481f6249ae049f81cbbb4
DavyVan <affqqa@163.com>
2020-02-23 20:54:23 -0800

    Just changed for loop before METIS to uint32_t

--- src/groute/graphs/csr_graph.cpp
+++ src/groute/graphs/csr_graph.cpp
@@ -62,14 +62,14 @@ namespace graphs {
 
             // Convert to 64-bit for metis
             std::vector<idx_t> row_start (nnodes+1), edge_dst (nedges), edge_weights;
-            for (int i = 0; i < nnodes + 1; ++i)
+            for (uint32_t i = 0; i < nnodes + 1; ++i)
                 row_start[i] = static_cast<idx_t>(m_origin_graph.row_start[i]);
-            for (int i = 0; i < nedges; ++i)
+            for (uint32_t i = 0; i < nedges; ++i)
                 edge_dst[i] = static_cast<idx_t>(m_origin_graph.edge_dst[i]);
             if(m_origin_graph.edge_weights)
             {
                 edge_weights.resize(nedges);
-                for (int i = 0; i < nedges; ++i)
+                for (uint32_t i = 0; i < nedges; ++i)
                     edge_weights[i] = static_cast<idx_t>(m_origin_graph.edge_weights[i]);
             }
             printf("Converted graph to %d-bit, calling METIS\n", (int)IDXTYPEWIDTH);
